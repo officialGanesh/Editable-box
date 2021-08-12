8449df6c92b54bf6dc7a89badb4155f17c83941a
Ganesh <ganuofficial2795@gmail.com>
2021-08-12 12:33:23 +0530

    saved data

--- edit.js
+++ edit.js
@@ -1,2 +1,32 @@
 
-console.log("Writing 🖊️")
+console.log("Write 🖊️")
+
+// Grabbing some elements
+let title = document.querySelector("#title");
+let text = document.querySelector("#text");
+let save = document.querySelector("#save");
+// console.log(`title: ${title} \n text-content: ${text} \n save: ${save}`)
+
+// Saving data in local-storage
+save.addEventListener("click",function(){
+    
+    let str_title = title.value;
+    let str_text = text.value;
+
+    if(str_title==="" || str_text === ""){
+        console.log("Please! provide data ")
+    }
+    else{
+
+        var title_content = str_title;
+        if(localStorage.getItem("title_content") === ""){
+            console.log("Please! provide data to store")
+        }
+        else{
+            localStorage.setItem(title_content,str_text)
+        }
+    }
+
+
+})
+
